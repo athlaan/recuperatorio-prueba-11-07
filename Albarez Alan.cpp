b30cee51a60eb3ba0bc1a0c42664e153e0bcb233
アトランゼラ <102254054+athlaan@users.noreply.github.com>
2023-07-11 17:31:10 -0300

    Add files via upload

---
+++ Albarez Alan.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+//medicamento[numero - 1];
+int main(void){
+	int numero;
+	int medicamento[numero];
+	int precio[10] = {1000,200,5890,3410,4322,5311,7090,8123,9033,6110};
+	 
+	for(int i = 0; i < 10; i++){
+	printf("Ingrese El codigo del Medicamento, del 1 al 10\n");
+	scanf("%d", &numero);
+
+	}
+		
+	
+}
